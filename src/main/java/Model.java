9c5082e795fe7cfb554ea461c4fec7d6c57c4fca
AlmasSinev <26almassinev@gmail.com>
2022-10-03 18:43:15 +0300

    Game options

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -13,7 +13,15 @@ public class Model extends JPanel implements ActionListener {
     private final int BLOCK_SIZE = 24;
     private final int N_BLOCKS = 15;
     private final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
+    private final int MAX_GHOSTS = 12;
+    private final int PACMAN_SPEED = 6;
 
+    private int N_GHOSTS = 6;
+    private int lives, score;
+    private int[] dx, dy;
+    private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
+
+    private Image img;
 
     @Override
     public void actionPerformed(ActionEvent e) {
